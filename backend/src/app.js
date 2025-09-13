d893c3ec78ad452c2361e06e8a37f835619bd064
abhisheko5 <86157818+abhisheko5@users.noreply.github.com>
2025-09-13 17:06:56 +0530

    fix: Update CORS configuration to allow requests from Netlify deployment

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-  origin:"http://localhost:5173",
+  origin:["http://localhost:5173","https://dsatrackerwithai.netlify.app/"],
    credentials: true
 }));
 
